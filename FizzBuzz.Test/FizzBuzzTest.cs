3cf8a95e3ae484c1202984335be31a1324f7e32f
Jordan Dashel <jordan.dashel@gmail.com>
2015-04-17 09:05:50 -0700

    initial commit

---
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz.Test
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "buzz")]
+        [TestCase(6, "fizz")]
+        [TestCase(9, "fizz")]
+        [TestCase(10, "buzz")]
+        [TestCase(12, "fizz")]
+        [TestCase(15, "fizz buzz")]
+        public void Test(int value, string expected)
+        {
+            var result = FizzBuzzGame.GetResponse(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGame
+    {
+
+        public static string GetResponse(int number)
+        {
+            string response = FizzBuzzGame.BuildResponse(number);
+
+            return response;
+        }
+
+        private static string BuildResponse(int number)
+        {
+            string response = "";
+
+            if (IsMultipleOfThreeOrFive(number))
+            {
+                response += IsMultipleOfThree(number) ? "fizz" : "";
+                response += IsMultipleOfThreeAndFive(number) ? " " : "";
+                response += IsMultipleOfFive(number) ? "buzz" : "";
+            }
+            else
+            {
+                response += number.ToString();
+            }
+            return response;
+        }
+
+        private static bool IsMultipleOfThree(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        private static bool IsMultipleOfFive(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        private static bool IsMultipleOfThreeOrFive(int number)
+        {
+            return IsMultipleOfThree(number) || IsMultipleOfFive(number);
+        }
+
+        private static bool IsMultipleOfThreeAndFive(int number)
+        {
+            return IsMultipleOfThree(number) && IsMultipleOfFive(number);
+        }
+    }
+}
